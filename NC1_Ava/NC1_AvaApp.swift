40dc0a0b59feeb940f7408731977786ad6c918a5
vaa9 <psuihangya@student.ciputra.ac.id>
2023-05-04 13:20:10 +0700

    Initial Commit

---
+++ NC1_Ava/NC1_AvaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_AvaApp.swift
+//  NC1_Ava
+//
+//  Created by Pravangasta Suihangya Balqis Wahyudi on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct NC1_AvaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
